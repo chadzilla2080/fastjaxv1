7f0e9f19e1e5ca3187ad0cbac154386fc4e32c5e
Chad Buie <chadbuie@gmail.com>
2015-06-06 19:42:46 +0000

    Styled the loggin system devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   
+  # This creates the paths for our categories and subcategories 
+  resources :categories do
+    resources :subcategories
+  end
+  
+  # This is the login system
   devise_for :users
+  
   # This is for the home page
   root 'categories#index'
   
