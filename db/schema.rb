df68f19d621c51237b61291a46c928bf49690789
Chad Buie <chadbuie@gmail.com>
2015-06-13 18:12:15 +0000

    Added a migration to existing database for listings table, category id and sub category id.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150601041615) do
+ActiveRecord::Schema.define(version: 20150613180904) do
 
   create_table "categories", force: true do |t|
     t.string "name"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20150601041615) do
     t.string   "sideoftown"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
+    t.integer  "subcategory"
   end
 
   create_table "seotags", force: true do |t|
