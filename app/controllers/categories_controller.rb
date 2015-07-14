18663f38fef17635cd368352f1a53466943be049
Chad Buie <chadbuie@gmail.com>
2015-07-14 13:05:52 +0000

    Added new subcategories for main home view.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,8 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
     @air_conditioning = @categories[0]  
+    @human_services_category = @categories[0]
+    @real_estate_category = @categories[0]
   end
   
 end   
