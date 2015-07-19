7b6959c44e340c13e67d30d2e5986dbf337ed758
Chad Buie <chadbuie@gmail.com>
2015-07-19 23:54:29 +0000

    Updated my subcategories controller to build the view and built the show page. Also created breadcrumb nav to sub and categories.

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -1,2 +1,8 @@
 class SubcategoriesController < ApplicationController
+  
+  def show
+    @category = Category.find(params[:category_id])
+    @subcategory = Subcategory.find(params[:id])
+  end
+  
 end
